221d6f9662cf370d2888105693499475c1b6b3ab
Axolotl <samuelmachat@gmail.com>
2019-04-06 12:49:35 -0400

    successfully querying api and serving up data

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const cors = require('cors')
 const express = require('express')
+const axios = require('axios')
 const server = express()
 
 // configure vars
@@ -19,6 +20,21 @@ server.use(cors())
 // serve requests
 server.get('/', (req, res) => {
   return res.status(200).json({ message: 'hello!' })
+})
+
+server.get('/stations', (req, res) => {
+  axios
+    .get(`${api_url}/stations`, {
+      headers: { authorization: `Bearer ${api_secret}` }
+    })
+    .then(api_res => {
+      console.log(api_res)
+      res.status(200).json(api_res.data)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'oops! something went wrong' })
+    })
 })
 
 // listen for requests
